f60b43a27aac8a82c3948ceaca81387e86ecf03c
gouflv <lv.gouf@gmail.com>
2025-02-20 18:45:38 +0800

    example

--- packages/queue/example/package.ts
+++ packages/queue/example/package.ts
@@ -1,5 +1,13 @@
 import artifact from "../lib/utils/artifact"
 ;(async () => {
-  const path = await artifact.packageArtifacts(["example.png"], "pkg.zip")
+  const path = await artifact.packageArtifacts(
+    [
+      {
+        filename: "example.png",
+        distName: "dist.png",
+      },
+    ],
+    "pkg.zip",
+  )
   console.log(path)
 })()
