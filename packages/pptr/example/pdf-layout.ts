191c18c8ac73c2433d3eae345fc7e65be9de960c
gouflv <lv.gouf@gmail.com>
2025-02-28 15:23:21 +0800

    pptr example

--- packages/pptr/example/pdf-layout.ts
+++ packages/pptr/example/pdf-layout.ts
@@ -1,15 +1,14 @@
 import { promises as fs } from "fs"
 import { launch } from "../lib/browser"
-
 ;(async () => {
   const { context, close: closeAll } = await launch()
 
   const page = await context.newPage()
 
-  // NOTE: pdf has it's own viewport
-  // page.setViewport({ width: 440, height: 900 });
-
-  const url = new URL("./html/typography.html", import.meta.url)
+  //
+  // NOTE: 支持封面，静态内容的页眉、页脚，不支持页码
+  //
+  const url = new URL("./html/pdf-layout.html", import.meta.url)
 
   await page.goto(url.href)
 
@@ -18,10 +17,9 @@ import { launch } from "../lib/browser"
   } catch (e) {}
 
   await page.pdf({
-    path: "./out/typography.pdf",
-    width: 600,
-    height: 800,
+    path: "./out/pdf-layout.pdf",
     printBackground: true,
+    format: "A4",
   })
 
   await closeAll()
